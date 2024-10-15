e7b4814aebd4ea3802ff8ea8697c1a194f271bcd
cemal <cemaltoptas55@gmail.com>
2024-10-15 10:00:47 +0200

    updated line 16

--- src/test/java/com/oop/runners/CukesRunner.java
+++ src/test/java/com/oop/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/oop/step_definitions",
         dryRun = false,
-        tags = " @checkPageTitle",
+        tags = "@smoke",
         monochrome = true
 )
 
